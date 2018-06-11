2db837f08e8d32c8f742fd0109aa9e8cda556a0f
Randy Oram <oramrandy@gmail.com>
2018-06-11 13:38:50 -0500

    Update WeatherApp.js

--- WeatherApp.js
+++ WeatherApp.js
@@ -1,1 +1,20 @@
+var weather;
+function setup()
+{
+	createCanvas(100%,100%);
+	loadJSON("api.openweathermap.org/data/2.5/forecast?q=Houston&APPID=1af755e1dccfae12d193c56c18b8f9bf",gotData);
+}
 
+function gotData(data)
+{
+	weather = data;
+}
+
+function draw()
+{
+	background(0);
+	if (weather)
+	{
+		ellipse(100,100,weather.main.temp,weather.main.temp);
+	}
+}
