640a3100d29958798a3ac760dddbdc2f5a772938
Randy Oram <oramrandy@gmail.com>
2018-06-11 17:16:19 -0500

    Update WeatherApp.js

--- WeatherApp.js
+++ WeatherApp.js
@@ -6,8 +6,9 @@ function main()
 	    xhttp.open("POST", "https://api.darksky.net/forecast/fb2905b05b877d0a1983b5c982316c88/30.1658,-95.4613", true);
 	    xhttp.setRequestHeader("Content-type", "application/json");
 	    xhttp.send();
-	    var response = JSON.parse(xhttp.responseText);
 	    alert('testing');
+	    var response = JSON.parse(xhttp.responseText);
+	    
 	    draw(response);
 	});
 }
