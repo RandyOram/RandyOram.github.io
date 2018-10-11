2dd2e98bf4de09e7889f7c606c09cc1773de1a95
fergusonmir <miranda.diana.ferguson@gmail.com>
2018-10-11 11:29:57 -0400

    added header and set default to dark map

--- PicApp/index.js
+++ PicApp/index.js
@@ -8,20 +8,10 @@ async function loadmap()
 var geojson;
 
 L.mapbox.accessToken = 'pk.eyJ1Ijoic21pdHR5dyIsImEiOiJjam1xamowNnkxcW43M3FueXdya3lmeWM1In0.FLBJa6hbAK1_9T-xgusgBQ';
-var map = L.mapbox.map('map', 'mapbox.streets');
-L.mapbox.styleLayer('mapbox://styles/mapbox/streets-v9').addTo(map);
+var map = L.mapbox.map('map', 'mapbox.dark');
+//L.mapbox.styleLayer('mapbox://styles/mapbox/streets-v9').addTo(map);
 
-var layerList = document.getElementById('menu');
-var inputs = layerList.getElementsByTagName('input');
 
-function switchLayer(layer) {
-  var layerId = layer.target.id;
-  L.mapbox.styleLayer('mapbox://styles/mapbox/' + layerId + '-v9').addTo(map);
-}
-
-for (var i = 0; i < inputs.length; i++) {
-  inputs[i].onclick = switchLayer;
-}
 
 map.on('load', () => {
   loadmap().then(result => {
