75692f767c8b61a3937b599997cac94dd7b4df1a
Randy Oram <oramrandy@gmail.com>
2018-10-16 09:47:55 -0400

    Updated index.js

--- PicApp/index.js
+++ PicApp/index.js
@@ -1,57 +1,28 @@
-mapboxgl.accessToken = 'pk.eyJ1Ijoic21pdHR5dyIsImEiOiJjam1xamowNnkxcW43M3FueXdya3lmeWM1In0.FLBJa6hbAK1_9T-xgusgBQ';
-var map = new mapboxgl.Map({
-    container: 'map',
-    style: 'mapbox://styles/mapbox/basic-v9',
-    zoom: 2,
-    center: [3.7038, 40.4168]
-});
-
-var layerList = document.getElementById('menu');
-var inputs = layerList.getElementsByTagName('input');
-
-function switchLayer(layer) {
-    var layerId = layer.target.id;
-    map.setStyle('mapbox://styles/mapbox/' + layerId + '-v9');
+async function loadmap()
+{
+  let result = await fetch('https://api.myjson.com/bins/1do25w');
+  let geojson = await(result.json());
+  return geojson;
 }
 
-for (var i = 0; i < inputs.length; i++) {
-    inputs[i].onclick = switchLayer;
-}
+var geojson;
 
-var geojson = {
-  type: 'FeatureCollection',
-  features: [{
-    type: 'Feature',
-    geometry: {
-      type: 'Point',
-      coordinates: [-77.032, 38.913]
-    },
-    properties: {
-      title: 'Mapbox',
-      description: 'Washington, D.C.'
-    }
-  },
-  {
-    type: 'Feature',
-    geometry: {
-      type: 'Point',
-      coordinates: [-122.414, 37.776]
-    },
-    properties: {
-      title: 'Mapbox',
-      description: 'San Francisco, California'
-    }
-  }]
-};
+L.mapbox.accessToken = 'pk.eyJ1Ijoic21pdHR5dyIsImEiOiJjam1xamowNnkxcW43M3FueXdya3lmeWM1In0.FLBJa6hbAK1_9T-xgusgBQ';
+var map = L.mapbox.map('map', 'mapbox.dark');
+//L.mapbox.styleLayer('mapbox://styles/mapbox/streets-v9').addTo(map);
 
-geojson.features.forEach(function(marker) {
 
-  // create a HTML element for each feature
-  var el = document.createElement('div');
-  el.className = 'marker';
 
-  // make a marker for each feature and add to the map
-  new mapboxgl.Marker(el)
-  .setLngLat(marker.geometry.coordinates)
-  .addTo(map);
-});
+// map.on('load', () => {
+//   loadmap().then(result => {
+//     geojson = result;
+//     geojson.users.forEach(function(user)
+//     {
+//       user.photos.forEach(function(photo)
+//       {
+//         L.marker(photo.coordinates).bindPopup(`<img src=${photo.url} alt="" height="84" width="84">`).addTo(map);
+
+//       });
+//     });
+//   });
+// });
