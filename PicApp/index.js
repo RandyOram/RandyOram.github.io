98db1fb980d1c04bdadb6f335a2b6dab371aae08
dylanalvarez1 <dylanalvarez1@ufl.edu>
2018-10-18 10:03:49 -0400

    reimplement markers

--- PicApp/index.js
+++ PicApp/index.js
@@ -1,6 +1,6 @@
 async function loadmap()
 {
-  let result = await fetch('https://api.myjson.com/bins/1do25w');
+  let result = await fetch('https://api.myjson.com/bins/hjwbs');
   let geojson = await(result.json());
   return geojson;
 }
@@ -9,20 +9,20 @@ var geojson;
 
 L.mapbox.accessToken = 'pk.eyJ1Ijoic21pdHR5dyIsImEiOiJjam1xamowNnkxcW43M3FueXdya3lmeWM1In0.FLBJa6hbAK1_9T-xgusgBQ';
 var map = L.mapbox.map('map', 'mapbox.dark');
-//L.mapbox.styleLayer('mapbox://styles/mapbox/streets-v9').addTo(map);
 
 
 
-// map.on('load', () => {
-//   loadmap().then(result => {
-//     geojson = result;
-//     geojson.users.forEach(function(user)
-//     {
-//       user.photos.forEach(function(photo)
-//       {
-//         L.marker(photo.coordinates).bindPopup(`<img src=${photo.url} alt="" height="84" width="84">`).addTo(map);
 
-//       });
-//     });
-//   });
-// });
+map.on('load', () => {
+  loadmap().then(result => {
+      geojson = result;
+      geojson.users.forEach(function(user)
+      {
+        user.photos.forEach(function(photo)
+      {
+        let marker = L.marker([photo.coordinates[0], photo.coordinates[1]]).bindPopup(`<img src=${photo.url} alt="" height="84" width="84">`);
+        marker.addTo(map);
+      });
+     });
+   });
+ });
